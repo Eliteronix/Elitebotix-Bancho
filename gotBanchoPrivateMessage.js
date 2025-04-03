9ba50f83d399ad7cde9e25c344a15798c875f43b
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-04 01:52:30 +0200

    Moved np to seperate command

--- gotBanchoPrivateMessage.js
+++ gotBanchoPrivateMessage.js
@@ -1,6 +1,3 @@
-const { getUserDuelStarRating, getValidTournamentBeatmap } = require('./utils');
-const { DBElitebotixProcessQueue, DBElitebotixDiscordUsers } = require('./dbObjects');
-const { getOsuPP, getOsuBeatmap, getMods } = require(`${process.env.ELITEBOTIXROOTPATH}/utils`);
 const fs = require('fs');
 
 // Replace utils and client dependencies
@@ -28,14 +25,18 @@ module.exports = async function (bancho, message) {
 		}
 	}
 
-	//Listen to now playing / now listening and send pp info
-	if (message.message.match(/https?:\/\/osu\.ppy\.sh\/beatmapsets\/.+\/\d+/gm)) {
-		return nowListening(bancho, message);
-	}
-
 	let args = message.message.slice(1).trim().split(/ +/);
 
 	let commandName = args.shift().toLowerCase();
+
+	//Listen to now playing / now listening and send pp info
+	if (message.message.match(/https?:\/\/osu\.ppy\.sh\/beatmapsets\/.+\/\d+/gm)) {
+		nowListening(bancho, message);
+
+		let command = bancho.commands.find(cmd => cmd.name === 'np');
+
+		return command.execute(bancho, message, args);
+	}
 
 	//Set the command and check for possible uses of aliases
 	let command = bancho.commands.find(cmd => cmd.name === commandName)
@@ -47,64 +48,3 @@ module.exports = async function (bancho, message) {
 
 	command.execute(bancho, message, args);
 };
-
-async function nowListening(bancho, message) {
-	let beatmapId = message.message.match(/https?:\/\/osu\.ppy\.sh\/beatmapsets\/.+\/\d+/gm)[0].replace(/.+\//gm, '');
-
-	let modBits = 0;
-
-	if (message.message.includes('-NoFail')) {
-		modBits += 1;
-	}
-	if (message.message.includes('-Easy')) {
-		modBits += 2;
-	}
-	if (message.message.includes('+Hidden')) {
-		modBits += 8;
-	}
-	if (message.message.includes('+HardRock')) {
-		modBits += 16;
-	}
-	if (message.message.includes('+DoubleTime')) {
-		modBits += 64;
-	}
-	if (message.message.includes('-HalfTime')) {
-		modBits += 256;
-	}
-	if (message.message.includes('+Nightcore')) {
-		modBits += 512 + 64; //Special case
-	}
-	if (message.message.includes('+Flashlight')) {
-		modBits += 1024;
-	}
-	if (message.message.includes('+SpunOut')) {
-		modBits += 4096;
-	}
-	if (message.message.includes('+FadeIn')) {
-		modBits += 1048576;
-	}
-	if (message.message.includes('+KeyCoop')) {
-		modBits += 33554432;
-	}
-
-	let beatmap = await getOsuBeatmap({ beatmapId: beatmapId, modBits: modBits });
-
-	console.log(`Got beatmap ${beatmap.beatmapId} from ${message.user.username} (${message.user.id})`);
-
-	bancho.lastUserMaps[message.user.id.toString()] = { beatmapId: beatmapId, modBits: modBits };
-
-	let firstPP = await getOsuPP(beatmap.beatmapId, null, beatmap.mods, 95.00, 0, beatmap.maxCombo);
-	let secondPP = await getOsuPP(beatmap.beatmapId, null, beatmap.mods, 98.00, 0, beatmap.maxCombo);
-	let thirdPP = await getOsuPP(beatmap.beatmapId, null, beatmap.mods, 99.00, 0, beatmap.maxCombo);
-	let fourthPP = await getOsuPP(beatmap.beatmapId, null, beatmap.mods, 100.00, 0, beatmap.maxCombo);
-
-	let mods = getMods(beatmap.mods);
-
-	if (!mods[0]) {
-		mods = ['NM'];
-	}
-
-	mods = mods.join('');
-
-	message.user.sendMessage(`[https://osu.ppy.sh/b/${beatmap.beatmapId} ${beatmap.artist} - ${beatmap.title} [${beatmap.difficulty}]] [${mods}] | 95%: ${Math.round(firstPP)}pp | 98%: ${Math.round(secondPP)}pp | 99%: ${Math.round(thirdPP)}pp | 100%: ${Math.round(fourthPP)}pp | ♫${beatmap.bpm} CS${beatmap.circleSize} AR${beatmap.approachRate} OD${beatmap.overallDifficulty} HP${beatmap.hpDrain}`);
-}
