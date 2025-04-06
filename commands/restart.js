aa594bcef525a139f9f8dc02c1f3b867c971ad2b
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-06 16:12:05 +0200

    Fixed restart command

--- commands/restart.js
+++ commands/restart.js
@@ -1,13 +1,15 @@
+const { trySendMessage } = require("../utils");
+
 module.exports = {
 	name: 'restart',
 	help: '!restart [force] - Restarts the bot',
 	async execute(bancho, message, args) {
 		console.log(bancho.autoHosts);
 		if (args[0] && args[0].toLowerCase() === 'force') {
-			await message.reply('Restarting bot...');
+			await trySendMessage(message.user, 'Restarting bot...');
 			process.exit(0);
 		} else {
-			await message.reply('Restarting bot when possible...');
+			await trySendMessage(message.user, 'Restarting bot when possible...');
 			bancho.update = 1;
 		}
 	},
