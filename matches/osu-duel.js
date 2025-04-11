8e8da6e602206393df89142bfe4f448f9132c73e
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-11 20:45:40 +0200

    Update osu-duel.js

--- matches/osu-duel.js
+++ matches/osu-duel.js
@@ -3,6 +3,7 @@ const { logMatchCreation, updateQueueChannels, addMatchMessage, restartIfPossibl
 const { getUserDuelStarRating, saveOsuMultiScores, getNextMap, humanReadable } = require(`${process.env.ELITEBOTIXROOTPATH}/utils`);
 const osu = require('node-osu');
 const { osuFilterWords } = require('../config.json');
+const { Op } = require('sequelize');
 
 module.exports = {
 	async execute(bancho, interaction, averageStarRating, lowerBound, upperBound, bestOf, onlyRanked, users, queued) {
@@ -755,4 +756,22 @@ async function getOsuMapInfo(dbBeatmap) {
 	}
 
 	return tournamentString;
+}
+
+function shuffle(array) {
+	let currentIndex = array.length, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
 }
