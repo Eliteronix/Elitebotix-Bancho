82b8e37c48f47ee3973053004d77b9fb8cdb6a38
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-11 20:28:47 +0200

    Create osu-duel.js

---
+++ processQueueTasks/osu-duel.js
@@ -0,0 +1,11 @@
+const osuDuel = require('../matches/osu-duel.js');
+
+module.exports = {
+	async execute(bancho, processQueueEntry) {
+		let settings = JSON.parse(processQueueEntry.additions);
+
+		osuDuel.execute(bancho, settings.interaction, settings.averageStarRating, settings.lowerBound, settings.upperBound, settings.bestOf, settings.onlyRanked, settings.users, settings.queued);
+
+		processQueueEntry.destroy();
+	},
+};
