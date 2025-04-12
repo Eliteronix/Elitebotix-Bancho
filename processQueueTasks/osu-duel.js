de15171aee7e67cbe802be14da1a40ee9c925780
Eliteronix <zimmermann.mariomarvin@gmail.com>
2025-04-12 17:04:04 +0200

    Update osu-duel.js

--- processQueueTasks/osu-duel.js
+++ processQueueTasks/osu-duel.js
@@ -4,8 +4,6 @@ module.exports = {
 	async execute(bancho, processQueueEntry) {
 		let settings = JSON.parse(processQueueEntry.additions);
 
-		console.log(settings);
-
 		osuDuel.execute(bancho, settings.interaction, settings.averageStarRating, settings.lowerBound, settings.upperBound, settings.bestOf, settings.onlyRanked, settings.users, settings.queued);
 
 		processQueueEntry.destroy();
